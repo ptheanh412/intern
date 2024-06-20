53b67d31939f33e25e1449ce0352c50edcef43b7
ptheanh412 <pham041203theanh@gmail.com>
2024-06-20 23:22:16 +0700

    level1

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@styles': path.resolve(__dirname, 'src/styles')
+    }
+  }
+});
